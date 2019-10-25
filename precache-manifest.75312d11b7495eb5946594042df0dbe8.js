05d9c3b7f607ccb513b362124cfdf5703ba88e11
vic <victordjohnson@gmail.com>
2019-10-25 12:22:49 -0500

    Updates

---
+++ precache-manifest.75312d11b7495eb5946594042df0dbe8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bc1a507912a2d0834c30c6c1c1575be",
+    "url": "/javascript-words-with-javascript/index.html"
+  },
+  {
+    "revision": "fd518459242bbac203fb",
+    "url": "/javascript-words-with-javascript/static/css/main.2639c90a.chunk.css"
+  },
+  {
+    "revision": "0d441c826652c6f0ca47",
+    "url": "/javascript-words-with-javascript/static/js/2.615960c1.chunk.js"
+  },
+  {
+    "revision": "fd518459242bbac203fb",
+    "url": "/javascript-words-with-javascript/static/js/main.a6077fb4.chunk.js"
+  },
+  {
+    "revision": "4c783e432032216276be",
+    "url": "/javascript-words-with-javascript/static/js/runtime-main.bab37129.js"
+  }
+]);
